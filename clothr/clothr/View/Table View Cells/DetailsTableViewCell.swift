a6ebc46aafd78e67681f84ba71ffb67f26a1d07f
Sam Singh <ssingh55@ucsc.edu>
2018-07-16 17:32:02 -0700

    updating xCode and mac

--- clothr/clothr/View/Table View Cells/DetailsTableViewCell.swift
+++ clothr/clothr/View/Table View Cells/DetailsTableViewCell.swift
@@ -1,25 +1,24 @@
 //
 //  DetailsTableViewCell.swift
-//  
+//
 //
 //  Created by Andrew Guterres on 12/17/17.
 //
-
 import UIKit
 
 class DetailsTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var sizeLabel: UILabel!
     @IBOutlet weak var colorLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
